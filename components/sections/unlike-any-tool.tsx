af0f1cd2ea93b9ed300d6c73154f12557a1bb96a
MLowenthal <lowenthal.maxwell@gmail.com>
2024-04-30 13:36:07 -0700

    four box mobile edit

--- components/sections/unlike-any-tool.tsx
+++ components/sections/unlike-any-tool.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Container } from "../container";
 import { BarChartIllustration } from "../illustrations/bar-chart";
 import { HubSpotLogo } from "../illustrations/hubspot-logo";
@@ -18,25 +19,25 @@ export const UnlikeAnyTool = () => (
         </p>
       </div>
     </Container>
-    <div className="h-[48rem] overflow-hidden md:h-auto md:overflow-visible">
+    <div className="overflow-hidden md:h-auto">
       <div className="flex snap-x snap-mandatory gap-6 overflow-x-auto px-8 pb-12 md:flex-wrap md:overflow-hidden">
-        <div className="relative flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-end overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:p-14">
+        <div className="relative flex w-full shrink-0 snap-center flex-col items-center justify-end overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:p-14">
           <KeyboardShortcuts />
           <p className="mb-4 text-3xl">Discover new clients</p>
           <p className="text-md text-primary-text">
-            I've  <a className="underline" href="https://docs.google.com/spreadsheets/d/1lAczUu_2sleG91ns2FNP9QeY7wOLLQRNrZUmOpa89r8/edit?usp=sharing" target="_blank" rel="noopener noreferrer">identified &gt;100 ideal fit customers</a> for Linear's sales team using public data.
+            I've <a className="underline" href="https://docs.google.com/spreadsheets/d/1lAczUu_2sleG91ns2FNP9QeY7wOLLQRNrZUmOpa89r8/edit?usp=sharing" target="_blank" rel="noopener noreferrer">identified &gt;100 ideal fit customers</a> for Linear's sales team using public data.
           </p>
         </div>
-        <div className="relative flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-end overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:basis-[calc(33.33%-12px)] md:p-14">
+        <div className="relative flex w-full shrink-0 snap-center flex-col items-center justify-end overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:basis-[calc(33.33%-12px)] md:p-14">
           <div className="absolute top-[-9.2rem] z-0">
-            <BarChartIllustration/>
+            <BarChartIllustration />
           </div>
           <p className="mb-4 text-3xl z-10 relative">Incredibly Precise</p>
           <p className="text-md text-primary-text z-10 relative">
             I've built <a className="underline" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/spreadsheets/d/1lmNvdc6ncLvChNYP_IXk11G6nXv96WDwMCT5EN2_X50/edit?usp=sharing">forecasts</a> and <a className="underline" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/spreadsheets/d/101g0H5ol6Q3K2Qfp0OfRNQnn1cMCkr35ipur6rUcHrQ/edit?usp=sharing">models</a> for every size business.
           </p>
         </div>
-        <div className="group relative flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-end overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:basis-[calc(33.33%-12px)] md:p-14">
+        <div className="group relative flex w-full shrink-0 snap-center flex-col items-center justify-end overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:basis-[calc(33.33%-12px)] md:p-14">
           <div className="pointer-events-none absolute top-[-8rem] w-[130%] z-0">
             <HubSpotLogo />
           </div>
@@ -45,14 +46,14 @@ export const UnlikeAnyTool = () => (
             I've negotiated, launched, and managed tools like Hubspot and Pocus.
           </p>
         </div>
-        <div className="relative flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-start overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:p-14">
+        <div className="relative flex w-full shrink-0 snap-center flex-col items-center justify-start overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:p-14">
           <div className="transition-opacity md:[.opened+&]:opacity-0">
             <p className="mb-4 text-3xl">Your Modern RevOps Expert</p>
             <p className="text-md text-primary-text">
               I can complete any request in record time using data + code.
             </p>
           </div>
-          <CardsChat/>
+          <CardsChat />
         </div>
       </div>
     </div>
