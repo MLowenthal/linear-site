39f6cf894a5f1769cfd414cae5d2d8e149c5ce0b
MLowenthal <lowenthal.maxwell@gmail.com>
2024-04-30 17:05:02 -0700

    l

--- components/sections/clients.tsx
+++ components/sections/clients.tsx
@@ -15,14 +15,28 @@ export const Clients = () => (
     </p>
 
     {/* Desktop version: Unchanged, logos in a row */}
-    <div className="flex flex-wrap justify-around gap-x-6 gap-y-8 [&_svg]:max-w-[16rem] [&_svg]:basis-[calc(50%-12px)] md:[&_svg]:basis-[calc(16.66%-20px)]">
+    <div className="hidden md:flex md:flex-wrap md:justify-around md:gap-x-6 md:gap-y-8 [&_svg]:md:max-w-[16rem] [&_svg]:md:basis-[calc(25%-12px)]">
       <LinkedInLogo url="https://linkedin.com" />
       <FNDLogo url="https://foundation.app" />
       <GDLogo url="https://www.glassdoor.com/index.htm" />
       <CSLogo url="https://www.youtube.com/@ColinandSamir" />
     </div>
 
-
+    {/* Mobile version: Logos in a 2x2 grid */}
+    <div className="grid grid-cols-2 gap-6 px-4 py-4 md:hidden [&_svg]:max-w-full [&_svg]:basis-1/2">
+      <div>
+        <LinkedInLogo url="https://linkedin.com" />
+      </div>
+      <div>
+        <FNDLogo url="https://foundation.app" />
+      </div>
+      <div>
+        <GDLogo url="https://www.glassdoor.com/index.htm" />
+      </div>
+      <div>
+        <CSLogo url="https://www.youtube.com/@ColinandSamir" />
+      </div>
+    </div>
   </>
 );
 
